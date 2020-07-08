acf0206b7607faa89b750d7c31f0ee92761beda6
leandromendes25 <leandromiendes@yahoo.com>
2020-07-08 09:34:43 -0300

    Service layer, component registration

---
+++ src/main/java/com/leandroprojeto/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.leandroprojeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leandroprojeto.entites.User;
+
+//UserRepository vai ser uma interfcace pq o Repository é uma interface também
+//tipo da entidade e o tipo do ID.
+//nao criamos uma implementação pq o JpaRepository ja tem uma implementação
+public interface UserRepository extends JpaRepository<User, Long>{
+
+
+
+}
