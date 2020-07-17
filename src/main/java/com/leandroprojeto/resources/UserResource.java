f76a5ab69a167f352c28650fec11701a506c190f
leandromendes25 <leandromiendes@yahoo.com>
2020-07-17 10:47:48 -0300

    User insert

--- src/main/java/com/leandroprojeto/resources/UserResource.java
+++ src/main/java/com/leandroprojeto/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.leandroprojeto.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leandroprojeto.entites.User;
 import com.leandroprojeto.services.UserService;
@@ -28,5 +32,15 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 	User obj = service.findById(id);
 	return ResponseEntity.ok().body(obj);
+	}
+	//ReponseEntity vai retornar o valor inserido
+	
+	@PostMapping//para inserir dados, Transformar de json para o objto user.
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		 obj = service.insert(obj);//no Path vai conter o barra users + o caminho id que for inserir. BuilAndExpand esperar o id que formos inserir
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				 .path("/{id}").buildAndExpand(obj.getId()).toUri();
+		 return ResponseEntity.created(uri).body(obj);
+		 //caminho.body. valor inserido
 	}
 }
--- src/main/java/com/leandroprojeto/services/UserService.java
+++ src/main/java/com/leandroprojeto/services/UserService.java
@@ -24,4 +24,8 @@ public User findById(Long id) {
 	Optional<User> obj = repository.findById(id);
 	return obj.get();
 }
+public User insert(User obj) {
+	return repository.save(obj);
+}
+
 }
